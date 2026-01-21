0882425bc4c774e47ecfef63479d150f5efb6922
Tayyibkara <tensarkara@gmail.com>
2026-01-21 15:43:38 +0100

    Removed old modular files, using new DisplayUtils and SystemUtils

---
+++ pic24fStarter-main/build/default/production/sensor.o.d
@@ -0,0 +1,12 @@
+build/default/production/sensor.o: sensor.c sensor.h PIC24FStarter.h \
+ SH1101A.h TouchSense.h RGBLeds.h
+
+sensor.h:
+
+PIC24FStarter.h:
+
+SH1101A.h:
+
+TouchSense.h:
+
+RGBLeds.h:
